a77c5d119f73f08f149bb5d311f2f0fa62596271
braifz <braian94lp@gmail.com>
2021-02-13 14:13:55 -0300

    new branch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,21 @@
 import React from 'react';
 import {Flex, Heading, Image, Box} from '@chakra-ui/react';
 import Picture from '../img/me.jpg';
+import '../styles/index.css'
 
 function App() {
   return (
-    
-    <Box display="flex"
-    flexDir="column"
-    justifyContent="center" alignItems="center"
-    minH="870px"
-    bg="#718096"
-    >
-      <Flex
-      bg="#2A4365"
-      w="40%"
-      h="500px"
-      p="1rem"
-      flexDir="column"
-      justify="center"
-      alignItems="center"
-      border="1px solid #2D3748"
-      boxShadow="5px 10px 18px #171923"
-      borderRadius="8px">
-        <Heading as="h1" size="4xl" textAlign="center" color="#fafafa">
-          Hola!
-        </Heading>
-        <Heading as="h2" size="2xl" textAlign="center" color="#fafafa" borderBottom="1px solid #fafafa">
-          Bienvenido a mi pagina Web! (en construcción)
-        </Heading>
-        <Heading as="h2" size="2xl" textAlign="center" color="#fafafa">
-          Mi nombre es Braian Fernandez 😃
-        </Heading>
-        <Heading as="h2" size="2xl" textAlign="center" color="#fafafa">
-          Soy desarrollador Front-End Jr.
-        </Heading>
-        <Image
-        boxSize="150px"
-        objectFit="cover"
-        src={Picture}
-        borderRadius="20px"
-        alt="braifz"
-        />
+    <Box>
 
-      </Flex>
-    </Box>
+   
+      <div class="bg"></div>
+      <div class="bg bg2"></div>
+      <div class="bg bg3"></div>
+      <div class="content">
+        
+      </div>
+    
+  </Box>
   );
 }
 
