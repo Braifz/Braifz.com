9dac19a301baa2623f9bfa67b840ac24d7f71083
braifz <braian94lp@gmail.com>
2022-03-30 12:40:01 -0300

    create footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer className='footer-container'>
+      <div className='col col-1'>
+        <h1 className='text'>footer</h1>
+        <h1 className='text'>header</h1>
+        <h1 className='text'>header</h1>
+        <h1 className='text'>header</h1>
+      </div>
+      <div className='col col-2'>
+        <h1 className='text'>footer</h1>
+        <h1 className='text'>header</h1>
+        <h1 className='text'>header</h1>
+        <h1 className='text'>header</h1>
+      </div>
+      <div className='col col-3'>
+        <h1 className='text'>footer</h1>
+        <h1 className='text'>header</h1>
+        <h1 className='text'>header</h1>
+        <h1 className='text'>header</h1>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
