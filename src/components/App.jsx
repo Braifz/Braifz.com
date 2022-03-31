c559d20a9bab5a6179df19048d59d39df37d7b38
Braifz <braian94lp@gmail.com>
2022-03-31 14:38:57 -0300

    refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,15 @@
 import React from 'react';
-import Header from './Header/Header';
-import Footer from './Footer/Footer'
+import Layout from './Layout/Layout';
+import PresentationCard from './PresentationCard/PresentationCard';
 
-import PictureMe from '../assets/img/melessmb.jpg';
 import '../styles/index.css';
-import githubicon from '../assets/img/github.png';
-import linkedinicon from '../assets/img/linkedin.png';
 function App() {
-  const English = {
-    greeting: ' Welcome! :D',
-    aboutme: 'Im Braian, Front-End dev Jr.',
-    ilive: 'I live in Buenos Aires, Argentina',
-    ilike: 'I like the cats, drink coffee and doing acrobatics',
-    techs: 'Techs what i use are Html, Css , Javascript , React and Git ',
-    button: '🇦🇷 ',
-  };
-
   return (
     <>
-      <Header />
-      <main className='container'>
-        <div className='bg'></div>
-        <div className='bg bg2'></div>
-        <div className='bg bg3'></div>
-        <p className='greeting'> {English.greeting}</p>
-        <div class='picture-container'>
-          <img className='picture-me' src={PictureMe} alt='me ' />
-        </div>
-        <div className='text-container'>
-          <p className='text--style'>{English.aboutme}</p>
-          <p className='text--style'>{English.ilive}</p>
-          <p className='text--style'>{English.ilike}</p>
-          <p className='text--style'>{English.techs}</p>
-        </div>
-        <div className='buttom-links'>
-          <div className='github-icon'>
-            <a href='https://github.com/Braifz'>
-              <img src={githubicon} alt='github' />
-            </a>
-          </div>
-          <div className='linkedin-icon'>
-            <a href='https://www.linkedin.com/in/braian-fernandez-ba90291a8/'>
-              <img src={linkedinicon} alt='linkedin' />
-            </a>
-          </div>
-        </div>
-      </main>
-      <Footer/>
-      </>
+      <Layout>
+        <PresentationCard />
+      </Layout>
+    </>
   );
 }
 
