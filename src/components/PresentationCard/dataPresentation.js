12e3fe089a6bdcd57a481208cd2ee85c3e5a0781
Braifz <braian94lp@gmail.com>
2022-04-01 02:46:02 -0300

    delete old data

--- src/components/PresentationCard/dataPresentation.js
+++
@@ -1,8 +0,0 @@
-export const dataPresentation = {
-  greeting: ' Welcome! :D',
-  aboutme: 'Im Braian, Front-End dev Jr.',
-  ilive: 'I live in Buenos Aires, Argentina',
-  ilike: 'I like the cats, drink coffee and doing acrobatics',
-  techs: 'Techs what i use are Html, Css , Javascript , React and Git ',
-  button: '🇦🇷 ',
-};
