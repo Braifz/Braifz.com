56eb0a2fcd5fd7c8e32f4b4cf285e5e82591a3a0
Braifz <braian94lp@gmail.com>
2022-04-01 02:49:33 -0300

    change data and styles in presentationCard

--- src/components/PresentationCard/PresentationCard.jsx
+++ src/components/PresentationCard/PresentationCard.jsx
@@ -1,22 +1,19 @@
-import { dataPresentation } from './dataPresentation';
-import PictureMe from '../../assets/img/melessmb.jpg';
+import PictureMe from '../../assets/img/me-recortado.jpeg';
 import './PresentationCard.css';
+import { otherDataPresentation } from './otherDataPresentation';
 
 const PresentationCard = () => {
   return (
     <main className='container'>
-      <div className='bg'></div>
-      <div className='bg bg2'></div>
-      <div className='bg bg3'></div>
-      <p className='greeting'> {dataPresentation.greeting}</p>
-      <div class='picture-container'>
-        <img className='picture-me' src={PictureMe} alt='me ' />
-      </div>
-      <div className='text-container'>
-        <p className='text--style'>{dataPresentation.aboutme}</p>
-        <p className='text--style'>{dataPresentation.ilive}</p>
-        <p className='text--style'>{dataPresentation.ilike}</p>
-        <p className='text--style'>{dataPresentation.techs}</p>
+      <div className='container-title'>
+        <div className='picture-container'>
+          <img className='picture-me' src={PictureMe} alt='img' />
+        </div>
+        <div className='text-container'>
+          <h1 className='title'>Hola! Soy Braian :D </h1>
+          <h3 className='subtitle'>Un poco sobre mi...</h3>
+          <p className='text--style'>{otherDataPresentation}</p>
+        </div>
       </div>
     </main>
   );
