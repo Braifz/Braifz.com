209cf6c49ede8c3d32e2c2853a38d13442b805b8
Braifz <braian94lp@gmail.com>
2022-04-01 02:47:26 -0300

    create component Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
+import './Layout.css';
+
 const Layout = ({ children }) => {
   return (
     <main>
       <Header />
-      <div>{children}</div>
+      <div className='children-container'>{children}</div>
       <Footer />
     </main>
   );
