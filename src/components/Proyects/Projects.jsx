3ed2301c6159381df14a837076ad063ec0124b86
Braifz <braian94lp@gmail.com>
2022-04-01 02:45:42 -0300

    create component project and styles

---
+++ src/components/Proyects/Projects.jsx
@@ -0,0 +1,12 @@
+import './Projects.css';
+
+const Proyects = () => {
+  return (
+    <div className='proyects-container'>
+      <h1 className='text-proyects'>Proyectos</h1>
+      <p> en construccion...</p>
+    </div>
+  );
+};
+
+export default Proyects;
