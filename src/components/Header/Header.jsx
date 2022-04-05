17b4cf49c982f880cecb22c33377ed62582aade8
Braifz <braian94lp@gmail.com>
2022-04-04 22:53:58 -0300

    Header components and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,9 @@ import './Header.css';
 const Header = () => {
   return (
     <header className='header-container'>
-      <h1>FRONTEND DEVELOPER</h1>
+      <div>
+        <h1>FRONTEND DEVELOPER</h1>
+      </div>
     </header>
   );
 };
