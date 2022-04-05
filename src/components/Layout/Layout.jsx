f19429e2d6c97ae30fd89c531d45550d60a9ffa0
Braifz <braian94lp@gmail.com>
2022-04-04 22:54:38 -0300

    fix component Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,21 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { motion } from 'framer-motion/dist/es/index';
 
 import './Layout.css';
 
+const variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
+
 const Layout = ({ children }) => {
   return (
-    <main>
+    <motion.div initial='hidden' animate='visible' variants={variants}>
       <Header />
       <div className='children-container'>{children}</div>
       <Footer />
-    </main>
+    </motion.div>
   );
 };
 
