8dec4108b12285da612a38afc96f9f0343245e67
braifz <braian94lp@gmail.com>
2022-04-08 08:37:32 -0300

    change structure projects components

--- src/components/Proyects/Projects.jsx
+++ src/components/Proyects/Projects.jsx
@@ -1,10 +1,14 @@
+import ProjectCard from './ProjectCard';
+import {dataProjectCard} from './dataProjectCard.js'
 import './Projects.css';
 
 const Proyects = () => {
   return (
     <div className='proyects-container'>
       <h1 className='text-proyects'>Proyectos</h1>
-      <p> en construccion...</p>
+      <ProjectCard data={dataProjectCard}/>
+      <ProjectCard data={dataProjectCard}/>
+      <ProjectCard data={dataProjectCard}/>
     </div>
   );
 };
