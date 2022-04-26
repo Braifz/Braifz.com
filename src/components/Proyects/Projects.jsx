1b2cc1e040c89dddd5de0796fa202f5bae0231f4
Braifz <braian94lp@gmail.com>
2022-04-26 20:44:59 -0300

    quit components test

--- src/components/Proyects/Projects.jsx
+++ src/components/Proyects/Projects.jsx
@@ -1,14 +1,12 @@
-import ProjectCard from './ProjectCard';
-import {dataProjectCard} from './dataProjectCard.js'
-import './Projects.css';
+import ProjectCard from "./ProjectCard";
+import { dataProjectCard } from "./dataProjectCard.js";
+import "./Projects.css";
 
 const Proyects = () => {
   return (
-    <div className='proyects-container'>
-      <h1 className='text-proyects'>Proyectos</h1>
-      <ProjectCard data={dataProjectCard}/>
-      <ProjectCard data={dataProjectCard}/>
-      <ProjectCard data={dataProjectCard}/>
+    <div className="proyects-container">
+      <h1 className="text-proyects">Proyectos</h1>
+      <ProjectCard data={dataProjectCard} />
     </div>
   );
 };
