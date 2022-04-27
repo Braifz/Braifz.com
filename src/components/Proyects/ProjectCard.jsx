3c6ac99ef825fdf6167523a029d6b41e135fd1ee
Braifz <braian94lp@gmail.com>
2022-04-26 21:01:58 -0300

    change component card

--- src/components/Proyects/ProjectCard.jsx
+++ src/components/Proyects/ProjectCard.jsx
@@ -1,21 +1,35 @@
-import './ProjectCard.css'
+import braifzImg from "../../assets/img/capture-braifz.png";
+import "./ProjectCard.css";
 
-const ProjectCard = ({data}) => {
-    return(
-        <div className="card-container">
-            <div className="title-container">
-                <h1>{data.title}</h1>
-            </div>
-            <div className="stack-container">
-                <p className="stack">{data.stack}</p>
-                <img src="https://placekitten.com/200/200" alt="kitty-img" />
-            </div>
-            <ul className="links-container">
-                <li><a href={data.github}>Github</a></li>
-                <li><a href={data.deploy}>braifz.com</a></li>
-            </ul>
-        </div>
-    )
-}
+const ProjectCard = ({ data }) => {
+  //data -> image,title,stack,github,deploy,descr
+  return (
+    <div className="card-container">
+      <img src={braifzImg} alt="proyect-img" />
+      <h2 className="card-title">{data.title}</h2>
+      <p>{data.description}</p>
+      <h3>Techs</h3>
+      <ul className="card-stack-container">
+        {data.stack.map((el, i) => (
+          <li className="card-stack-item" key={i}>
+            {el}
+          </li>
+        ))}
+      </ul>
+      <ul className="card-links-container">
+        <li>
+          <a className="card-links" href={data.github}>
+            Github
+          </a>
+        </li>
+        <li>
+          <a className="card-links" href={data.deploy}>
+            Deploy
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default ProjectCard;
