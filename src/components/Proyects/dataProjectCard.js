c1c89943d417226e699e4315ed7b001fbdfa317d
Braifz <braian94lp@gmail.com>
2022-05-17 18:37:32 -0300

    add new project

--- src/components/Proyects/dataProjectCard.js
+++ src/components/Proyects/dataProjectCard.js
@@ -1,10 +1,21 @@
 import braifzImg from "../../assets/img/capture-braifz.png";
+import todoApp from "../../assets/img/todo-app.png";
 
-export const dataProjectCard = {
-  title: "Braifz.com",
-  image: { braifzImg },
-  stack: ["React js", "Framer motion", "CSS3", "Formik"],
-  description: "Personal portfolio",
-  github: "https://github.com/Braifz",
-  deploy: "https://www.braifz.com/",
-};
+export const dataProjectCard = [
+  {
+    title: "Braifz.com",
+    image: braifzImg,
+    stack: ["React js", "Framer motion", "CSS3", "Formik"],
+    description: "Personal portfolio",
+    github: "https://github.com/Braifz/Braifz.com",
+    deploy: "https://www.braifz.com/",
+  },
+  {
+    title: "To-Do List App",
+    image: todoApp,
+    stack: ["React js", "styled-components", "Context", "Formik"],
+    description: "To-Do List App",
+    github: "https://github.com/Braifz/todo-app",
+    deploy: "https://todo-app-sigma-virid.vercel.app/",
+  },
+];
