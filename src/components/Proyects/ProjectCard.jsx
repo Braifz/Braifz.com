fd1b1c0e3787f9e5e2900d126b10ce18d75fd442
Braifz <braian94lp@gmail.com>
2022-05-17 18:36:22 -0300

    change logic component

--- src/components/Proyects/ProjectCard.jsx
+++ src/components/Proyects/ProjectCard.jsx
@@ -1,11 +1,9 @@
-import braifzImg from "../../assets/img/capture-braifz.png";
 import "./ProjectCard.css";
 
 const ProjectCard = ({ data }) => {
-  //data -> image,title,stack,github,deploy,descr
   return (
     <div className="card-container">
-      <img src={braifzImg} alt="proyect-img" />
+      <img src={data.image} alt="proyect-img" />
       <h2 className="card-title">{data.title}</h2>
       <p>{data.description}</p>
       <h3>Techs</h3>
@@ -18,12 +16,22 @@ const ProjectCard = ({ data }) => {
       </ul>
       <ul className="card-links-container">
         <li>
-          <a className="card-links" href={data.github}>
+          <a
+            className="card-links"
+            href={data.github}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Github
           </a>
         </li>
         <li>
-          <a className="card-links" href={data.deploy}>
+          <a
+            className="card-links"
+            href={data.deploy}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Deploy
           </a>
         </li>
